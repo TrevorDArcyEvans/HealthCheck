6969e1e03d765a777da5bb9155a2292eda708d8d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-12-01 15:01:46 +0000

    - initial version
    -- based on https://www.telerik.com/blogs/checking-health-aspnet-core-apis
    - inject typed HttpClient for better lifetime management

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,36 @@
+﻿namespace HealthCheck.Controllers
+{
+  using System.Net;
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.Extensions.Diagnostics.HealthChecks;
+  using Microsoft.Extensions.Logging;
+  using Newtonsoft.Json;
+
+  [ApiController]
+  [Route("[controller]")]
+  public sealed class HealthController : ControllerBase
+  {
+    private readonly ILogger<HealthController> _logger;
+    private readonly HealthCheckService _service;
+
+    public HealthController(
+      ILogger<HealthController> logger,
+      HealthCheckService service)
+    {
+      _logger = logger;
+      _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      var report = await _service.CheckHealthAsync();
+      var reportToJson = JsonConvert.SerializeObject(report, Formatting.None);
+
+      _logger.LogInformation($"Get Health Information: {reportToJson}");
+
+      return report.Status == HealthStatus.Healthy ? Ok(reportToJson) : StatusCode((int)HttpStatusCode.ServiceUnavailable, reportToJson);
+    }
+  }
+}
---
+++ Custom/CatHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿namespace HealthCheck.Custom
+{
+  using System.Net;
+  using System.Net.Http;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+  public sealed class CatHealthCheck : IHealthCheck
+  {
+    private readonly HttpClient _client;
+
+    public CatHealthCheck(HttpClient client)
+    {
+      _client = client;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+      var response = await _client.GetAsync("", cancellationToken);
+
+      return response.StatusCode == HttpStatusCode.OK
+        ? await Task.FromResult(new HealthCheckResult(
+          status: HealthStatus.Healthy,
+          description: "The API is healthy （。＾▽＾）"))
+        : await Task.FromResult(new HealthCheckResult(
+          status: HealthStatus.Unhealthy,
+          description: "The API is sick (‘﹏*๑)"));
+    }
+  }
+}
